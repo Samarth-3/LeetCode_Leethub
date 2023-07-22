698f50646e07fe55c6eca71161a2f56146399a93
Samarth Paliwal <99992354+Samarth-3@users.noreply.github.com>
2023-07-22 18:31:17 +0530

    Time: 18 ms (18.73%), Space: 5.9 MB (54.32%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int fib(int n) {
-        int sum=0;
         if(n==0||n==1){
             return n;
         }
-        
-        sum+=fib(n-1)+fib(n-2);
-        return sum;
+    
+       return fib(n-1)+fib(n-2);
     }
 };
