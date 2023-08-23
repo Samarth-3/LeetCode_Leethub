3c96d735f6fd31818d187d9435916a512a04acff
Samarth Paliwal <99992354+Samarth-3@users.noreply.github.com>
2023-08-23 22:18:00 +0530

    Time: 22 ms (59.11%), Space: 16.5 MB (11.71%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,38 +1,68 @@
 class MinStack {
-    int size;
-    int* arr;
-    int tops; 
 public:
+    stack<int> st, minimum;
     MinStack() {
-        size = 100001;
-        arr = new int[size];
-        tops = -1;
+        
     }
     
     void push(int val) {
-        if (tops == size - 1) {
-            cout << "stack Full";   
-        } else {
-            tops++;
-            arr[tops] = val;
+        if (minimum.empty()) minimum.push(val);
+        else {
+            minimum.push(min(val, minimum.top()));
         }
+        st.push(val);
     }
     
     void pop() {
-        tops--;
+        st.pop();
+        minimum.pop();
     }
     
     int top() {
-        return arr[tops];
+        return st.top();
     }
     
     int getMin() {
-        int min = arr[0];
-        for (int i = 1; i <= tops; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-        }
-        return min;
+        return minimum.top();
     }
 };
+
+
+// class MinStack {
+//     int size;
+//     int* arr;
+//     int tops; 
+// public:
+//     MinStack() {
+//         size = 100001;
+//         arr = new int[size];
+//         tops = -1;
+//     }
+    
+//     void push(int val) {
+//         if (tops == size - 1) {
+//             cout << "stack Full";   
+//         } else {
+//             tops++;
+//             arr[tops] = val;
+//         }
+//     }
+    
+//     void pop() {
+//         tops--;
+//     }
+    
+//     int top() {
+//         return arr[tops];
+//     }
+    
+//     int getMin() {
+//         int min = arr[0];
+//         for (int i = 1; i <= tops; i++) {
+//             if (arr[i] < min) {
+//                 min = arr[i];
+//             }
+//         }
+//         return min;
+//     }
+// };
