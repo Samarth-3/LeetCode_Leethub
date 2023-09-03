8769e2beb7fc6728c7c9fdb2f2265ecef3555b1b
Samarth Paliwal <99992354+Samarth-3@users.noreply.github.com>
2023-09-03 22:48:40 +0530

    Time: 106 ms (83.35%), Space: 128.3 MB (18.47%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int currGain = 0, totalGain = 0, answer = 0;
+        int deficit = 0, balance = 0, start = 0;
         
-        for (int i = 0; i < gas.size(); ++i) {
-            // gain[i] = gas[i] - cost[i]
-            totalGain += gas[i] - cost[i];
-            currGain += gas[i] - cost[i];
-            
-            // If we meet a "valley", start over from the next station
-            // with 0 initial gas.
-            if (currGain < 0) {
-                answer = i + 1;
-                currGain = 0;
+        for (int i = 0; i < gas.size(); i++) {
+            balance+=gas[i]-cost[i];
+            if(balance<0){
+                deficit+=balance;
+                start=i+1;
+                balance=0;
             }
         }
         
-        return totalGain >= 0 ? answer : -1;
+        if(deficit+balance>=0){
+            return start;
+        }
+        else{
+            return -1;
+        }
     }
 };
