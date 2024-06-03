2fc5f239fc816275f5a56e82b3ea6a909878ac69
Samarth Paliwal <99992354+Samarth-3@users.noreply.github.com>
2024-06-03 23:15:29 +0530

    Time: 15 ms (81.53%), Space: 16 MB (81.45%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,55 +1,58 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        queue<int>q;
-        vector<int>color(graph.size(),-1);
-        for(int i=0;i<graph.size();i++){
-            if(color[i] == -1){
+//     bool isBipartite(vector<vector<int>>& graph) {
+//         queue<int>q;
+//         vector<int>color(graph.size(),-1);
+//         for(int i=0;i<graph.size();i++){
+//             if(color[i] == -1){
                 
-                q.push(i);
-                color[i]=0;
-                while(!q.empty()){
-                    int node = q.front();
-                    q.pop();
-                    for(auto it: graph[node]){
-                        // if the adjacent color is yet not colored
-                        if(color[it] == -1){
-                            color[it] = !color[node];
-                            q.push(it);
-                        }
-                        else{
-                            if(color[it]==color[node])return false;
-                        }
-                    }
-                }
-            }
-        }return true;
-    }
-};
+//                 q.push(i);
+//                 color[i]=0;
+//                 while(!q.empty()){
+//                     int node = q.front();
+//                     q.pop();
+//                     for(auto it: graph[node]){
+//                         // if the adjacent color is yet not colored
+//                         if(color[it] == -1){
+//                             color[it] = !color[node];
+//                             q.push(it);
+//                         }
+//                         else{
+//                             if(color[it]==color[node])return false;
+//                         }
+//                     }
+//                 }
+//             }
+//         }return true;
+//     }
+
 
 //dfs sol
 
-// bool dfs(int start,int col, vector<vector<int>>&graph, vector<int>&color){
-//         color[start] = col;
-//         for(auto &it: graph[start]){
-//             if(color[it]==-1){
-//                 if(dfs(it,!col,graph,color)==false) return false;
-//             }
-//             else{
-//                 if(color[it] == col) return false;
-//             }
-//         }
-//         return true;
-//     }
-//     bool isBipartite(vector<vector<int>>& graph) {
-//         int n = graph.size();
-//         vector<int>color(n,-1);
-//         for(int i=0;i<n;i++){
-//             if(color[i]==-1){
-//                 if(dfs(i,0,graph,color)==false){
-//                     return false;
-//                 }
-//             }
-//         }
-//         return true;
-//     }
+bool dfs(int start,int col, vector<vector<int>>&graph, vector<int>&color){
+        color[start] = col;
+        for(auto &it: graph[start]){
+            if(color[it]==-1){
+                if(dfs(it,!col,graph,color)==false){
+                    return false;
+                };
+            }
+            else{
+                if(color[it] == col) return false;
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int>color(n,-1);
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                if(dfs(i,0,graph,color)==false){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
