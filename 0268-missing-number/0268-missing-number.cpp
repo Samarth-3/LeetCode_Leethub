2d68adb2c25628ff29c461cda7f9131691e6c383
Samarth Paliwal <99992354+Samarth-3@users.noreply.github.com>
2024-06-20 17:06:37 +0530

    Time: 11 ms (81.41%), Space: 20.4 MB (57.35%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,15 +1,31 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         int size=nums.size();
+//         for(int i=0;i<size;i++){
+//             if(i==size-1 && nums[i]==i){
+//                 return i+1;
+//             }
+//             if(i==nums[i] ) {continue;}
+//             return i;
+//         }
+//         return -1;
+//     }
+// };
+
+//better optimal solution
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int size=nums.size();
-        for(int i=0;i<size;i++){
-            if(i==size-1 && nums[i]==i){
-                return i+1;
-            }
-            if(i==nums[i] ) {continue;}
-            return i;
+        int n = nums.size();
+        int expected_sum = n * (n + 1) / 2;
+        int actual_sum = 0;
+
+        for (int num : nums) {
+            actual_sum += num;
         }
-        return -1;
+
+        return expected_sum - actual_sum;
     }
 };
