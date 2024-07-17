82b31952b879822838be963e4f22f6379a7bdcdf
Samarth Paliwal <99992354+Samarth-3@users.noreply.github.com>
2024-07-17 15:34:22 +0530

    Time: 14 ms (81.90%), Space: 26.9 MB (39.46%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate = nums[0];
-        int count = 1;
-        // Find the majority element
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] == candidate) {
+        int count = 0;
+        int candidate = 0;
+        
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            
+            if (num == candidate) {
                 count++;
             } else {
                 count--;
-                if (count == 0) {
-                    candidate = nums[i];
-                    count = 1;
-                }
             }
         }
         
         return candidate;
     }
 };
-//other sol is
-//return nums[n/2]
