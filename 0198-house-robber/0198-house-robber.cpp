9b804a0fb3855389e3e56b42ae6c2fced554c8aa
Samarth Paliwal <99992354+Samarth-3@users.noreply.github.com>
2024-08-10 11:52:27 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (64.39%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,3 +1,5 @@
+// tabulation code
+/*
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -12,5 +14,26 @@ public:
             dp[i]=max(pick,nopick);
         }
         return dp[n-1];
+    }
+};
+*/
+
+// space optimised;
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        int prev1=nums[0];
+        int prev2=0;
+
+        for(int i=1;i<n;i++){
+            int pick=nums[i];
+            if(i>1) pick+=prev2;
+            int nopick=0+prev1;
+            int curi=max(pick,nopick);
+            prev2=prev1;
+            prev1=curi;
+        }
+        return prev1;
     }
 };
